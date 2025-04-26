090d2f593f9b32016a7eb5107217f27501750277
Woo Shaun <wooshaun53@gmail.com>
2025-04-26 23:27:58 +0800

    add quick json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from "axios"
 import Note from "./components/Note"
 
 const Notes = ({ notes }) => {
@@ -15,10 +16,61 @@ const Notes = ({ notes }) => {
 }
 
 
-
-const App = ({ notes }) => {
+const AppLocal = ({ notes }) => {
   const [newNote, setNewNote] = useState("a new note...")
   const [currNote, setCurrNote] = useState(notes)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("default event submission prevented")
+
+    const noteObj = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: String(currNote.length + 1),
+    }
+
+    setCurrNote(currNote.concat(noteObj))
+    setNewNote("")
+  }
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <p>NOTES:</p>
+      <ul>
+        <li>Hello</li>
+        {currNote.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={newNote}
+          onChange={handleChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+const App = () => {
+  const [newNote, setNewNote] = useState("a new note...")
+  const [currNote, setCurrNote] = useState([])
+
+  const fetchDataFromServer = () => {
+    axios.get("http://localhost:3001/notes")
+      .then(response => {
+        setCurrNote(response.data)
+      })
+  }
+
+  useEffect(fetchDataFromServer, [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
