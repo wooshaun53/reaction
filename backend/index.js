a304ba0795586267c29607b7b49ba16ffc20854e
Woo Shaun <wooshaun53@gmail.com>
2025-05-01 21:44:24 +0800

    add mongodb support to backend

--- backend/index.js
+++ backend/index.js
@@ -3,42 +3,66 @@ const express = require('express')
 const app = express()
 
 app.use(cors())
+//app.use(express.static("dist"))
 
-let notes = [
-	{
-		id: "1",
-		content: "HTML is easy",
-		important: true
-	},
-	{
-		id: "2",
-		content: "Browser can execute only JavaScript",
-		important: false
-	},
-	{
-		id: "3",
-		content: "GET and POST are the most important methods of HTTP protocol",
-		important: true
-	}
-]
+//let notes = [
+//	{
+//		id: "1",
+//		content: "HTML is easy",
+//		important: true
+//	},
+//	{
+//		id: "2",
+//		content: "Browser can execute only JavaScript",
+//		important: false
+//	},
+//	{
+//		id: "3",
+//		content: "GET and POST are the most important methods of HTTP protocol",
+//		important: true
+//	}
+//]
+
+const mongoose = require('mongoose')
+
+// DO NOT SAVE PASSWORD TO GITHUB!!
+const password = process.argv[2]
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+	content: String,
+	important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-	response.json(notes)
+	//response.json(notes)
+	Note.find({}).then(notes => {
+		response.json(notes)
+	})
 })
 
 app.get('/api/notes/:id', (request, response) => {
 	const id = request.params.id
-	const note = notes.find(note => note.id === id)
+	//const note = notes.find(note => note.id === id)
 
-	if (note) {
+	Note.findById(id).then(note => {
 		response.json(note)
-	} else {
-		response.status(404).end()
-	}
+	})
+
+	//if (note) {
+	//	response.json(note)
+	//} else {
+	//	response.status(404).end()
+	//}
 })
 
 const generateId = () => {
@@ -59,15 +83,24 @@ app.post('/api/notes', (request, response) => {
 		})
 	}
 
-	const note = {
+	//const note = {
+	//	content: body.content,
+	//	important: body.important || false,
+	//	id: generateId(),
+	//}
+
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
-		id: generateId(),
-	}
+	})
+	//notes = notes.concat(note)
 
-	notes = notes.concat(note)
+	note.save().then(savedNote => {
+		response.json(savedNote)
+	})
 
-	response.json(note)
+
+	//response.json(note)
 })
 
 const PORT = process.env.PORT || 3001
