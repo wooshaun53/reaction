a505359a090da2f609d3950caa97d9ed1ded3ab4
Woo Shaun <wooshaun53@gmail.com>
2025-05-07 22:14:17 +0800

    add initial backend test

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,4 +1,7 @@
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = { PORT, MONGODB_URI };
